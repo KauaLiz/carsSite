39747eeddb471b9998f17276598fca4fe9506a7b
Kauã de Liz <kauadeliz@gmail.com>
2026-04-18 13:36:40 -0300

    add: var calcuSideAnimation to move thh car imagem dependeding on the button clicked

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let container = document.querySelector(".container");
 let items = container.querySelectorAll(".list .item");
 let indicator = document.querySelector(".indicators");
 let dots = indicator.querySelectorAll("ul li");
+let root = document.documentElement;
 
 let active = 0;
 let firstPosition = 0;
@@ -23,12 +24,14 @@ function setSlider(){
 }
 
 nextButton.onclick = () => {
+    root.style.setProperty("--calcuSideAnimation", 1);
     active = active + 1 > lastPosition ? 0 : active + 1;
     setSlider();
     items[active].classList.add("active");
 }
 
 prevButton.onclick = () => {
+    root.style.setProperty("--calcuSideAnimation", -1);
     active = active - 1 < 0 ? lastPosition : active - 1;
     setSlider();
     items[active].classList.add("active");
